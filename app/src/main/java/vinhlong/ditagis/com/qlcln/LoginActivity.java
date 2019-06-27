66033a2b016ac05f7d88206070960001dd005790
HongNguyen <hongnguyen060895@gmail.com>
2019-06-27 10:22:44 +0700

    ..

--- app/src/main/java/vinhlong/ditagis/com/qlcln/LoginActivity.java
+++ app/src/main/java/vinhlong/ditagis/com/qlcln/LoginActivity.java
@@ -24,6 +24,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
